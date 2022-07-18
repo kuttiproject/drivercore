7decc015d4ca8d7219c7a851d0bc5c50dc019ae8
Raj Chaudhuri <rajch@hotmail.com>
2022-07-18 14:44:21 +0530

    Documented: mock driver does not store image status permanently.
    Close #1.

--- drivercoretest/drivermock/doc.go
+++ drivercoretest/drivermock/doc.go
@@ -1,3 +1,7 @@
 // Package drivermock implements a mock driver. It should be used
 // only in tests.
+//
+// In particular, the driver implemented in this package uses mock
+// images, whose status is not stored permanently. Image status,
+// therefore, can vary between tests.
 package drivermock
